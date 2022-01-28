d370580e3970df95aa2e0b0e0e620be75ccdcc88
nsewnath <nsewnath@ufl.edu>
2022-01-27 21:36:51 -0500

    testing bs4

--- Plants_AWG/Working_Code/API_Pull_and_Reformat_Python/api_pull.py
+++ Plants_AWG/Working_Code/API_Pull_and_Reformat_Python/api_pull.py
@@ -1,1 +1,53 @@
+"""
+Script for Data Scraping and Reformating from GeneLab API
+
+Neeka Sewnath
+nsewnath@ufl.edu
+
+# Note: Must be run with Python 3.x
+"""
+
+#===========================================================================================================================================
+
 from bs4 import BeautifulSoup
+import requests
+import argparse
+
+#===========================================================================================================================================
+
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='API data scrape and reformat',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-u',
+                        '--url',
+                        help = 'url of data',
+                        metavar = 'url',
+                        type = str,
+                        default = "https://visualization.genelab.nasa.gov/GLOpenAPI/samples/?study.characteristics.organism=Arabidopsis%20thaliana&file.datatype=unnormalized%20counts&format=browser")
+
+
+    return parser.parse_args()
+
+#===========================================================================================================================================
+
+def main():
+
+    # Fetch arguments 
+    args = get_args()
+    url = args.url
+
+    # Fetch raw html content
+    html_content = requests.get(url).text
+
+    # Parse html content
+    soup = BeautifulSoup(html_content, "lxml")
+
+
+#===========================================================================================================================================
+
+if __name__ == '__main__':
+    main()
